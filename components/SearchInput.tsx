71c2d17513f9bedeabaa3bace9f9645d0d0299da
tn-booboo <ttnbooboo@gmail.com>
2026-05-15 15:35:38 +0700

    seacrh bar visibility

--- components/SearchInput.tsx
+++ components/SearchInput.tsx
@@ -32,11 +32,16 @@ const styles = StyleSheet.create({
   container: {
     flexDirection: 'row',
     alignItems: 'center',
-    backgroundColor: '#F2F2F7',
+    backgroundColor: '#FFFFFF',
     borderRadius: 12,
     marginHorizontal: 16,
     marginVertical: 12,
     paddingHorizontal: 12,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.1,
+    shadowRadius: 4,
+    elevation: 3,
   },
   icon: {
     fontSize: 16,
